079bec1218b211ae585734125573a880bfcde45c
Anthony Pruniere <anthony.pruniere@wanadoo.fr>
2025-09-22 10:55:55 +0200

    Implement dark mode toggle functionality and update styles for improved user experience

--- src/main.js
+++ src/main.js
@@ -1,2 +1,8 @@
 import './style.css'
 
+const darkModeBtn = document.getElementById("darkModeBtn");
+const body = document.body;
+
+darkModeBtn.addEventListener("click", () => {
+    body.classList.toggle("darkMode");    
+})
