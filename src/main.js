5364e2483e9b0c80a94f2038134fd5666bf02f96
Anthony Pruniere <anthony.pruniere@wanadoo.fr>
2025-09-22 16:56:13 +0200

    Add dropdown menu

--- src/main.js
+++ src/main.js
@@ -2,7 +2,25 @@ import './style.css'
 
 const darkModeBtn = document.getElementById("darkModeBtn");
 const body = document.body;
+const dropDown = document.getElementById("myDropDown");
 
 darkModeBtn.addEventListener("click", () => {
-    body.classList.toggle("darkMode");    
+    body.classList.toggle("darkMode");  
 })
+
+function myFunction(){
+    dropDown.classList.toggle("show");
+}
+
+window.onclick = function(event){
+    if (!event.target.matches(".dropBtn")){
+        var dropdown = document.getElementsByClassName("dropdownContent");
+        var i;
+        for (let i=0; i < dropdown.lenght; i++ ) {
+            var openDropdown = dropdown[i];
+            if(openDropdown.classList.contains("show")){
+                openDropdown.classList.remove("show");
+            }
+        }
+    }
+}
