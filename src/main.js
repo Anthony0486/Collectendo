75049f68d36e2a067e7c19fd7bf5985da4814c1d
Anthony Pruniere <anthony.pruniere@wanadoo.fr>
2025-10-03 16:25:45 +0200

    add modal contact

--- src/main.js
+++ src/main.js
@@ -4,7 +4,10 @@ const darkModeBtn = document.getElementById("darkModeBtn");
 const body = document.body;
 const menuBtn = document.getElementById("menuBtn");
 const dropdown = document.getElementById("myDropDown");
-const aPropos = document.getElementsByClassName("aPropos");
+const menuBtn1 = document.getElementById("menuBtn1");
+const dropdown1 = document.getElementById("myDropDown1");
+
+
 
 //DARK MODE
 darkModeBtn.addEventListener("click", () => {
@@ -15,19 +18,35 @@ darkModeBtn.addEventListener("click", () => {
 menuBtn.addEventListener("click", () => {
     dropdown.classList.toggle("show");
 });
+menuBtn1.addEventListener("click", () => {
+    dropdown1.classList.toggle("show");
+});
+window.addEventListener("click", (event) => {
+  if (!event.target.closest(".dropDown")) {
+    dropdown.classList.remove("show");
+  };
+});
+window.addEventListener("click", (event) => {
+  if (!event.target.closest(".dropDown")) {
+    dropdown1.classList.remove("show");
+  };
+});
 
-// aPropos.addEventListener("click", ()=>{
-//     dropdown.classList.toggle("show");
-// });
 
-window.onclick = function(event) {
-    if (!event.target.matches(".dropBtn")){
-        const dropdowns = document.getElementsByClassName("dropdownContent");
-        for (let i=0; i < dropdowns.length; i++ ) {
-            const openDropdown = dropdowns[i];
-            if(openDropdown.classList.contains("show")){
-                openDropdown.classList.remove("show");
-            };
-        };
-    };
-};
+//MODALE A PROPOS
+const contact = document.querySelector(".lienContact");
+const modal = document.getElementById("myModal");
+const btn = document.getElementById("myBtn");
+const span = document.getElementsByClassName("close")[0];
+
+contact.addEventListener("click", () => {
+  modal.style.display = "block";
+});
+span.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+window.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+});
