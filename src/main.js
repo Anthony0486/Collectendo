e9f07cee4e9e87644bbe623b2c7b54fd29eb8708
Anthony Pruniere <anthony.pruniere@wanadoo.fr>
2025-10-03 12:03:04 +0200

    modifs CSS

--- src/main.js
+++ src/main.js
@@ -4,16 +4,21 @@ const darkModeBtn = document.getElementById("darkModeBtn");
 const body = document.body;
 const menuBtn = document.getElementById("menuBtn");
 const dropdown = document.getElementById("myDropDown");
+const aPropos = document.getElementsByClassName("aPropos");
 
 //DARK MODE
 darkModeBtn.addEventListener("click", () => {
     body.classList.toggle("darkMode");  
-})
+});
 
-//DROPDOWN
+//DROPDOWN MENUS
 menuBtn.addEventListener("click", () => {
     dropdown.classList.toggle("show");
 });
+
+// aPropos.addEventListener("click", ()=>{
+//     dropdown.classList.toggle("show");
+// });
 
 window.onclick = function(event) {
     if (!event.target.matches(".dropBtn")){
@@ -22,7 +27,7 @@ window.onclick = function(event) {
             const openDropdown = dropdowns[i];
             if(openDropdown.classList.contains("show")){
                 openDropdown.classList.remove("show");
-            }
-        }
-    }
-}
+            };
+        };
+    };
+};
