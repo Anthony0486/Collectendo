27edf4ed28eaebde01ae1bd18bf91f54d38cfe44
Anthony Pruniere <anthony.pruniere@wanadoo.fr>
2025-10-09 17:12:22 +0200

    fix a propos modal, add search & my acount drop down menus

--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ const menuBtn = document.getElementById("menuBtn");
 const dropdown = document.getElementById("myDropDown");
 const menuBtn1 = document.getElementById("menuBtn1");
 const dropdown1 = document.getElementById("myDropDown1");
+const menuBtn2 = document.getElementById("menuBtn2");
+const dropdown2 = document.getElementById("myDropDown2");
+const menuBtn3 = document.getElementById("menuBtn3");
+const dropdown3 = document.getElementById("myDropDown3");
 
 
 
@@ -21,6 +25,12 @@ menuBtn.addEventListener("click", () => {
 menuBtn1.addEventListener("click", () => {
     dropdown1.classList.toggle("show");
 });
+menuBtn2.addEventListener("click", () => {
+    dropdown2.classList.toggle("show");
+});
+menuBtn3.addEventListener("click", () => {
+    dropdown3.classList.toggle("show");
+});
 window.addEventListener("click", (event) => {
   if (!event.target.closest(".dropDown")) {
     dropdown.classList.remove("show");
@@ -31,12 +41,37 @@ window.addEventListener("click", (event) => {
     dropdown1.classList.remove("show");
   };
 });
+window.addEventListener("click", (event) => {
+  if (!event.target.closest(".dropDown")) {
+    dropdown2.classList.remove("show");
+  };
+});
+window.addEventListener("click", (event) => {
+  if (!event.target.closest(".dropDown")) {
+    dropdown3.classList.remove("show");
+  };
+});
 
 
-//MODALE A PROPOS
+//MODALE QUI SUIS JE
+const quisuisje = document.querySelector(".lienquisuisje");
+const modal1 = document.getElementById("myModal1");
+const span1 = document.getElementsByClassName("close1")[0];
+
+quisuisje.addEventListener("click", () => {
+  modal1.style.display = "block";
+});
+span1.addEventListener("click", () => {
+  modal1.style.display = "none";
+});
+window.addEventListener("click", (event) => {
+  if (event.target === modal1) {
+    modal1.style.display = "none";
+  }
+});
+//MODALE CONTACT
 const contact = document.querySelector(".lienContact");
 const modal = document.getElementById("myModal");
-const btn = document.getElementById("myBtn");
 const span = document.getElementsByClassName("close")[0];
 
 contact.addEventListener("click", () => {
